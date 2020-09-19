6c87cb56922294a4b3bd45e01cd828532f3af059
Samzyte <melu2127@gmail.com>
2020-09-19 07:41:41 +0100

    React App Created

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'Shanna@melissa.tv'
+    },
+    {
+        id: 3,
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email: 'Nathan@yesnia.net'
+    },
+    {
+        id: 4,
+        name: 'Patricia Lebsack',
+        username: 'Karianne',
+        email: 'Julianne.OConner@kory.org'
+    },
+    {
+        id: 5,
+        name: 'Chelsey Dietrich',
+        username: 'Kamren',
+        email: 'Maxwell@jake.wap'
+    },
+    {
+        id: 6,
+        name: 'Mrs. Dennis Schulist',
+        username: 'Leo Cork',
+        email: 'Karl@jasper.info'
+    },
+    {
+        id: 7,
+        name: 'Kurtis Weissnat',
+        username: 'Stiles',
+        email: 'Telly@billy.biz'
+    },
+    {
+        id: 8,
+        name: 'Nicholas Runolfsdottir',
+        username: 'Maximus',
+        email: 'Sherwood@rosmond.me'
+    },
+    {
+        id: 9,
+        name: 'Glenna Reichert',
+        username: 'Delphine',
+        email: 'Chaim@mcdermott.io'
+    },
+    {
+        id: 10,
+        name: 'Clementina DuBuque',
+        username: 'Freddie',
+        email: 'Anthonio@cook.biz'
+    },
+
+]
